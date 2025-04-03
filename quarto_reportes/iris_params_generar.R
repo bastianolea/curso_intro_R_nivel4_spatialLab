b24cdf477107728f2e8f47c1e7c53bce3f3af245
bastianolea <bastianolea@gmail.com>
2025-04-03 17:18:25 -0300

    apuntes pre clase

--- quarto_reportes/iris_params_generar.R
+++ quarto_reportes/iris_params_generar.R
@@ -10,13 +10,27 @@ quarto_render(
 )
 
 
+# generar todos a la vez en html
 walk(c("virginica", "setosa", "versicolor"), ~{
   quarto_render(
-    input = "iris_params.qmd",
+    input = "quarto_reportes/iris_params.qmd",
     output_file = paste0("iris_params_", .x, ".html"),
     execute_params = list(
       especie = .x
     )
   )
 })
+
+
+# # generar todos a la vez en md para poder insertarlos
+# walk(c("virginica", "setosa", "versicolor"), ~{
+#   quarto_render(
+#     input = "quarto_reportes/iris_params.qmd",
+#     # output_format = "md",
+#     output_file = paste0("iris_params_", .x, ".md"),
+#     execute_params = list(
+#       especie = .x
+#     )
+#   )
+# })
 
